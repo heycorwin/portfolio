96b311675066d8b8463897a9a42ca00390460367
Corwin Harrell <hello@corwinharrell.com>
2024-07-25 03:38:37 -0400

    Update imports to include Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./styles/globalStyles.css";
 import styled from "styled-components";
 import Home from "./pages/Home";
