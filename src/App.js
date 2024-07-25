be85d9adbdda7076771074cd9fdc596b40a5150a
Corwin Harrell <hello@corwinharrell.com>
2024-07-25 04:12:13 -0400

    remove extra link import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
 import "./styles/globalStyles.css";
 import styled from "styled-components";
